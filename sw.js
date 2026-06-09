33b76079f3182ac2c11b44a427c2bcf944e04f80
true5h4d0w <marcus0reichelt@gmail.com>
2026-06-09 18:12:37 +0200

    Aktualisieren von sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    "cache-first" mit Netzwerk-Fallback und Laufzeit-Caching
    (z. B. für die Google-Schriftarten beim ersten Online-Start). */
 
-const CACHE = "slw-v6";
+const CACHE = "slw-v7";
 const CORE = [
   "./",
   "./index.html",
