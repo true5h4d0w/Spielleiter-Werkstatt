82ad7aa40b267a9de566c40b81085da97386f6ca
true5h4d0w <marcus0reichelt@gmail.com>
2026-06-10 14:30:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,18 @@
-const CACHE = "slw-v16";
-const CORE = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE = "slw-v17";
+const CORE = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()).catch(() => self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()).catch(()=>self.skipWaiting()));
 });
 self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener("fetch", e => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(caches.match(e.request).then(hit => {
-    if (hit) return hit;
-    return fetch(e.request).then(res => {
-      if (res && res.status === 200) { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {}); }
+  if(e.request.method!=="GET") return;
+  e.respondWith(caches.match(e.request).then(hit=>{
+    if(hit) return hit;
+    return fetch(e.request).then(res=>{
+      if(res&&res.status===200){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});}
       return res;
-    }).catch(() => caches.match("./index.html"));
+    }).catch(()=>caches.match("./index.html"));
   }));
 });
